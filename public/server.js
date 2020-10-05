8083da8f1aaf1c1ef795784b700a27c105181308
Christie Hui <cwsh15@gmail.com>
2020-10-05 01:05:13 -0700

    added download button

--- public/server.js
+++ public/server.js
@@ -26,9 +26,28 @@
           'Authorization': 'Bearer ' + access_token
         },
         success: function(response) {
-          console.log(response.items)
+          console.log(response.items);
+          $('#screen-container').show();
+          $('.thanks').show();
+
+          document.getElementById('download').addEventListener('click', function () {
+            var offScreen = document.querySelector('#screen-container');
+
+            window.scrollTo(0, 0);
+            html2canvas(offScreen).then((canvas) => {
+              var dataURL = canvas.toDataURL();
+              console.log(dataURL);
+              var link = document.createElement('a');
+              link.download = 'spotifysmashbros.png';
+              link.href = dataURL;
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);
+              delete link;
+            });
+          });
         }
-      });
+      })
     }
 
     var params = getHashParams();
@@ -52,6 +71,8 @@
 
               $('#login').hide();
               $('#loggedin').show();
+              $('#screen-container').hide();
+              $('.thanks').hide();
             }
         });
       } else {
@@ -62,13 +83,13 @@
 
       document.getElementById('short_term').addEventListener('click', function() {
         getArtists(access_token, 'short_term');                                       
-      });
+      }, false);
       document.getElementById('medium_term').addEventListener('click', function() {
         getArtists(access_token, 'medium_term');                                       
-      });
+      }, false);
       document.getElementById('long_term').addEventListener('click', function() {
         getArtists(access_token, 'long_term');                                       
-      });
+      }, false);
 
     }
   })();
